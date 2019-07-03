c0611090687dfbe44942bdda92c4bf368f01033b
Roope Keto <roope.keto@outlook.com>
2019-07-04 01:37:36 +0300

    cleaning data

--- models/tweetsentiment_naive_bayes.py
+++ models/tweetsentiment_naive_bayes.py
@@ -1,4 +1,15 @@
 import pandas as pd 
+import numpy as np
 
+# # # GET AND CLEAN THE DATA # # #
 data = pd.read_csv('/home/roope/projects/sort-by-sentiment/training-data/Sentiment Analysis Dataset.csv',
                    error_bad_lines=False)
+
+data['SentimentText'] = data['SentimentText'].apply(str.strip)
+
+# # # Using Bag of words method # # #
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform()
+
+
